cc5f5ecda114879be378b25646fc837b4e4788a0
Jh0mpis <jmorenotr@unal.edu.co>
2025-09-12 15:41:17 +0200

    edit!: Deleting the Symmetric Matmul, was valid just for commuting matrices.

--- PhotonSolverUtilities/src/include/Utilities.hxx
+++ PhotonSolverUtilities/src/include/Utilities.hxx
@@ -14,23 +14,6 @@
 #include <cctk.h>
 
 AMREX_GPU_DEVICE AMREX_GPU_HOST
-    CCTK_ATTRIBUTE_ALWAYS_INLINE inline const amrex::GpuArray<CCTK_REAL, 6>
-    SS_MatMul(const amrex::GpuArray<CCTK_REAL, 6> &A,
-              amrex::GpuArray<CCTK_REAL, 6> B) {
-
-  amrex::GpuArray<CCTK_REAL, 6> result =
-  { A[0] * B[0] + A[1] * B[1] + A[2] * B[2],
-    A[0] * B[1] + A[1] * B[3] + A[2] * B[4],
-    A[0] * B[2] + A[1] * B[4] + A[2] * B[5],
-    A[1] * B[1] + A[3] * B[3] + A[4] * B[4],
-    A[1] * B[2] + A[3] * B[4] + A[4] * B[5],
-    A[2] * B[2] + A[4] * B[4] + A[5] * B[5]};
-
-  return result;
-
-} // SS_MatMul
-
-AMREX_GPU_DEVICE AMREX_GPU_HOST
     CCTK_ATTRIBUTE_ALWAYS_INLINE inline const amrex::GpuArray<CCTK_REAL, 3>
     SMatVecMul(amrex::GpuArray<CCTK_REAL, 6> A,
                amrex::GpuArray<CCTK_REAL, 3> V) {
