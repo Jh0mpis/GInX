3a03c2694d9e5bd128fc96dd05db462a33027ab6
Jh0mpis <jmorenotr@unal.edu.co>
2025-09-24 15:31:31 +0200

    add: Fixing Doxygen documentation

--- docs/html/search/functions_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vector_5fbarycentric_5fderivative_0',['vector_barycentric_derivative',['../namespaceDiscretize.html#a287cc23d28bf219d4bdb1c3ecb147b31',1,'Discretize']]],
-  ['vecvecmul_1',['VecVecMul',['../Utilities_8hxx.html#a9422f8c771699b58ed5467ed1c64c378',1,'Utilities.hxx']]]
-];
